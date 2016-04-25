d02d167b384d264487e1bc564bea464e1450026b
Neraste <neraste.herr10@gmail.com>
2016-04-25 02:37:05 +0200

    Add artists and works models and serializers

--- dakara_server/library/models.py
+++ dakara_server/library/models.py
@@ -9,8 +9,54 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     file_path = models.CharField(max_length=255)
     duration = SafeDurationField(default=timedelta(0))
+    detail = models.CharField(max_length=255, blank=True)
+    artists = models.ManyToManyField('Artist')
+    works = models.ManyToManyField('Work', through='SongWorkLink')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.title)
+
+
+class Artist(models.Model):
+    """ Class for artists
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Work(models.Model):
+    """ Class for anime, games and so on that use songs
+    """
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class SongWorkLink(models.Model):
+    """ Class to describe the use of a song in a work
+    """
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    work = models.ForeignKey(Work, on_delete=models.CASCADE)
+    link_type_number = models.IntegerField(null=True)
+
+    OPENING = 'OP'
+    ENDING = 'ED'
+    INSERT = 'IN'
+    IMAGE = 'IS'
+    LINK_TYPE_CHOICES = (
+            (OPENING, "Opening"),
+            (ENDING, "Ending"),
+            (INSERT, "Insert song"),
+            (IMAGE, "Image somg"),
+            )
+    link_type = models.CharField(max_length=2, choices=LINK_TYPE_CHOICES)
+
+    def __str__(self):
+        return str(self.work.title) + ' ' + str(self.link_type) + \
+            (' ' + str(self.link_type_number) if self.link_type_number else '')
--- dakara_server/library/serializers.py
+++ dakara_server/library/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from library.models import Song
+from library.models import Song, Artist, Work, SongWorkLink
 
 
 class SecondsDurationField(serializers.DurationField):
@@ -12,10 +12,48 @@ class SecondsDurationField(serializers.DurationField):
         return str(int(round(obj.total_seconds())))
 
 
+class ArtistSerializer(serializers.ModelSerializer):
+    """ Class for artist serializer
+    """
+    class Meta:
+        model = Artist
+        fields = (
+                'name',
+                )
+
+
+class WorkSerializer(serializers.ModelSerializer):
+    """ Class for work serializer
+    """
+    class Meta:
+        model = Work
+        fields = (
+                'title',
+                'subtitle'
+                )
+
+
+class SongWorkLinkSerializer(serializers.ModelSerializer):
+    """ Class for serializing the use of a song in a work
+    """
+    work = WorkSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = SongWorkLink
+        fields = (
+                'work',
+                'link_type',
+                'link_type_number',
+                )
+
+
+
 class SongSerializer(serializers.HyperlinkedModelSerializer):
     """ Class for song serializer
     """
     duration = SecondsDurationField()
+    artists = ArtistSerializer(many=True, read_only=True)
+    works = SongWorkLinkSerializer(many=True, read_only=True, source='songworklink_set')
 
     class Meta:
         model = Song
@@ -25,6 +63,9 @@ class SongSerializer(serializers.HyperlinkedModelSerializer):
                 'title',
                 'file_path',
                 'duration',
+                'detail',
+                'artists',
+                'works',
                 'date_created',
                 'date_updated',
                 )
