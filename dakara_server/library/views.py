2a9c20c550ed879adaf9dd870da9563d69983bd5
Nadeflore <flore.love@gmail.com>
2016-06-06 01:10:31 +0200

    Search query with minilanguage

--- dakara_server/library/views.py
+++ dakara_server/library/views.py
@@ -4,6 +4,7 @@ from django.db.models.functions import Lower
 from library.models import Song, Artist, Work
 from library.serializers import SongSerializer
 from django.db.models import Q
+from .query_language import parse as parse_query
 
 
 class SongList(ListCreateAPIView):
@@ -16,11 +17,34 @@ class SongList(ListCreateAPIView):
         """
         if 'query' in self.request.query_params:
             query = self.request.query_params.get('query', None)
-            if query is not None:
+            if query:
+                res = parse_query(query)
+                q = []
+                for artist in res['artists']:
+                    q.append(Q(artists__name__icontains=artist))
+                for artist in res['artists_exact']:
+                    q.append(Q(artists__name__iexact=artist))
+                for work in res['works']:
+                    q.append(Q(works__title__icontains=work))
+                for work in res['works_exact']:
+                    q.append(Q(works__title__iexact=work))
+                for title in res['titles']:
+                    q.append(Q(title__icontains=title))
+                for title in res['titles_exact']:
+                    q.append(Q(title__iexact=work))
+                for remain in res['remaining']:
+                    q.append(
+                            Q(title__icontains=remain) |
+                            Q(artists__name__icontains=remain) |
+                            Q(works__title__icontains=remain)
+                        )
+
+                filter_query = q.pop()
+                for item in q:
+                    filter_query &= item
+
                 return Song.objects.filter(
-                        Q(title__icontains=query) |
-                        Q(artists__name__icontains=query) |
-                        Q(works__title__icontains=query)
+                        filter_query
                     ).order_by(Lower('title'))
 
         return Song.objects.all().order_by(Lower('title'))
