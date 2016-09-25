61076b39e96ac7c41c51da4b68f89ff1f7a8fe86
Neraste <neraste.herr10@gmail.com>
2016-09-25 20:17:56 +0200

    Add comments about song search process

--- dakara_server/library/views.py
+++ dakara_server/library/views.py
@@ -23,7 +23,7 @@ class LibraryPagination(PageNumberPagination):
                 'results': data,
             })
 
-    
+
 class SongList(ListCreateAPIView):
     """ Class for listing songs
     """
@@ -31,14 +31,21 @@ class SongList(ListCreateAPIView):
     pagination_class = LibraryPagination
 
     def get_queryset(self):
-        """ Filter the songs
+        """ Search and filter the songs
         """
+        # if 'query' is in the query string
+        # then perform search
         if 'query' in self.request.query_params:
             query = self.request.query_params.get('query', None)
             if query:
+                # the query can use a syntax, the query language, to specify
+                # which term to search where
+                # the language manages the simple search as well
+                # the parser is provided from query_language.py
                 res = parse_query(query)
                 q = []
                 q_many = []
+                # specific terms of the research, i.e. artists, works and titles
                 for artist in res['artists']:
                     q_many.append(Q(artists__name__icontains=artist))
                 for artist in res['artists_exact']:
@@ -51,29 +58,34 @@ class SongList(ListCreateAPIView):
                     q.append(Q(title__icontains=title))
                 for title in res['titles_exact']:
                     q.append(Q(title__iexact=title))
+                # unspecific terms of the research
                 for remain in res['remaining']:
                     q.append(
                             Q(title__icontains=remain) |
                             Q(artists__name__icontains=remain) |
                             Q(works__title__icontains=remain)
                         )
+
+                # tags
                 for tag in res['tags']:
                     q_many.append(Q(tags__name=tag))
 
+                # now, gather the query objects
                 filter_query = Q()
                 for item in q:
                     filter_query &= item
 
                 query_set = Song.objects.filter(filter_query)
-
+                # gather the query objects involving custom many to many relation
                 for item in q_many:
                     query_set = query_set.filter(item)
 
-                # saving the query to give it back to the client
+                # saving the parsed query to give it back to the client
                 self.query_parsed = res
 
                 return query_set.distinct().order_by(Lower('title'))
 
+        # otherwise return all songs
         return Song.objects.all().order_by(Lower('title'))
 
     def list(self, request, *args, **kwargs):
@@ -82,6 +94,9 @@ class SongList(ListCreateAPIView):
         response = super(SongList, self).list(request, args, kwargs)
 
         # pass the query words to highlight to the response
+        # the words have been passed to the object in the get_queryset method
+        # now, they have to be passed to the response
+        # this is why this function in overloaded
         if hasattr(self, 'query_parsed'):
             response.data['query'] = self.query_parsed
 
