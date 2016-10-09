53f0f6e529ccc1136f811e92019c97b3ad675862
Neraste <neraste.herr10@gmail.com>
2016-10-09 22:23:37 +0200

    Make query name for Work Type mandatory

--- dakara_server/library/models.py
+++ dakara_server/library/models.py
@@ -44,7 +44,7 @@ class WorkType(models.Model):
     """ Class for the type of a work: anime, games and so on 
     """
     name = models.CharField(max_length=255)
-    query_name = models.CharField(max_length=255)
+    query_name = models.CharField(max_length=255, unique=True)
     # icon_name refers to a fontawesome icon name
     icon_name = models.CharField(max_length=255, null=True)
 
