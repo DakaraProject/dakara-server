6047516ae25956d2c301c39974d511b4edd64e6f
Nadeflore <flore.love@gmail.com>
2017-09-09 15:35:38 +0200

    Add playlist tests

---
+++ dakara_server/playlist/tests_player.py
@@ -0,0 +1,321 @@
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .base_test import BaseAPITestCase
+from library.models import *
+from .models import *
+
+UserModel = get_user_model()
+
+class PlayerForUserViewTestCase(BaseAPITestCase):
+    url = reverse('playlist-player-status')
+    url_aggregated = reverse('playlist-player')
+
+    def setUp(self):
+        self.create_test_data()
+
+    def test_get_player_status(self):
+        """
+        Test to verify player status when nothing is playing
+        """
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Get player status
+        # Should be default player state
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['playlist_entry'], None)
+        self.assertEqual(response.data['timing'], 0)
+        self.assertEqual(response.data['paused'], False)
+
+        # Get player status again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status']['playlist_entry'], None)
+        self.assertEqual(response.data['status']['timing'], 0)
+        self.assertEqual(response.data['status']['paused'], False)
+
+    def test_get_player_status_playing(self):
+        """
+        Test to verify player status when playing
+        """
+        # Make player play next song in playlist at 23 seconds
+        playing_time = 23
+        self.player_play_next_song(playing_time)
+
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Get player status
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['playlist_entry']['id'], self.pe1.id)
+        self.assertEqual(response.data['timing'], playing_time)
+        self.assertEqual(response.data['paused'], False)
+
+        # Get player status again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status']['playlist_entry']['id'], self.pe1.id)
+        self.assertEqual(response.data['status']['timing'], playing_time)
+        self.assertEqual(response.data['status']['paused'], False)
+
+        # Make player continue playing the song, but at 47 seconds and paused
+        playing_time = 47
+        self.player_play_song(self.pe1.id, playing_time, True)
+
+        # Login as simple user
+        self.authenticate(self.user)
+
+        # Get player status
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['playlist_entry']['id'], self.pe1.id)
+        self.assertEqual(response.data['timing'], playing_time)
+        self.assertEqual(response.data['paused'], True)
+
+        # Get player status again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status']['playlist_entry']['id'], self.pe1.id)
+        self.assertEqual(response.data['status']['timing'], playing_time)
+        self.assertEqual(response.data['status']['paused'], True)
+
+        # Make player play next song in playlist at 2 seconds
+        playing_time = 2
+        self.player_play_next_song(playing_time)
+
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Get player status
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['playlist_entry']['id'], self.pe2.id)
+        self.assertEqual(response.data['timing'], playing_time)
+        self.assertEqual(response.data['paused'], False)
+
+        # Get player status again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status']['playlist_entry']['id'], self.pe2.id)
+        self.assertEqual(response.data['status']['timing'], playing_time)
+        self.assertEqual(response.data['status']['paused'], False)
+
+        # No more song in playlist, player should be idle
+        playing_time = 0
+        self.player_play_next_song(playing_time)
+
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Get player status
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['playlist_entry'], None)
+        self.assertEqual(response.data['timing'], playing_time)
+        self.assertEqual(response.data['paused'], False)
+
+        # Get player status again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status']['playlist_entry'], None)
+        self.assertEqual(response.data['status']['timing'], playing_time)
+        self.assertEqual(response.data['status']['paused'], False)
+
+
+class PlayerCommandForUserViewTestCase(BaseAPITestCase):
+    url = reverse('playlist-player-manage')
+    url_aggregated = reverse('playlist-player')
+
+    def setUp(self):
+        self.create_test_data()
+
+    def test_set_player_pause(self):
+        """
+        Test to test pausing player
+        """
+        # Play next song and check the player does not receive pause command
+        response = self.player_play_next_song()
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], False)
+
+        # Login as manager
+        self.authenticate(self.manager)
+
+        # Get current commands
+        # Should be default
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], False)
+
+        # Get player commands again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['manage']['pause'], False)
+        self.assertEqual(response.data['manage']['skip'], False)
+
+        # Request pause
+        response = self.client.put(self.url,
+                {
+                    'pause': True,
+                    'skip': False
+                }
+            )
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        # Get current commands
+        # Should be pause requested
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['pause'], True)
+        self.assertEqual(response.data['skip'], False)
+
+        # Get player commands again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['manage']['pause'], True)
+        self.assertEqual(response.data['manage']['skip'], False)
+
+
+        # Now, check player receive the pause request
+        response = self.player_play_next_song()
+        self.assertEqual(response.data['pause'], True)
+        self.assertEqual(response.data['skip'], False)
+
+    def test_set_player_pause_manager(self):
+        """
+        Test to test pausing player as manager
+        """
+        # Play next song
+        self.player_play_next_song()
+
+        # Login as manager
+        self.authenticate(self.manager)
+
+        # Request pause
+        # able to pause own entry
+        response = self.client.put(self.url,
+                {
+                    'pause': True,
+                    'skip': False
+                }
+            )
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        # Play next song
+        self.player_play_next_song()
+
+        # Login as manager
+        self.authenticate(self.manager)
+
+        # Request pause
+        # able to pause other's entry
+        response = self.client.put(self.url,
+                {
+                    'pause': True,
+                    'skip': False
+                }
+            )
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_set_player_pause_user(self):
+        """
+        Test to test pausing player as user
+        """
+        # Play next song
+        self.player_play_next_song()
+
+        # Login as playlist user
+        self.authenticate(self.p_user)
+
+        # Request pause
+        # not able to pause other's entry
+        response = self.client.put(self.url,
+                {
+                    'pause': True,
+                    'skip': False
+                }
+            )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Play next song
+        self.player_play_next_song()
+
+        # Login as playlist user
+        self.authenticate(self.p_user)
+
+        # Request pause
+        # able to pause own entry
+        response = self.client.put(self.url,
+                {
+                    'pause': True,
+                    'skip': False
+                }
+            )
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_set_player_skip(self):
+        """
+        Test to test skiping
+        """
+        # Play next song and check the player does not receive skip command
+        response = self.player_play_next_song()
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], False)
+
+        # Login as manager
+        self.authenticate(self.manager)
+
+        # Get current commands
+        # Should be default
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], False)
+
+        # Get player commands again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['manage']['pause'], False)
+        self.assertEqual(response.data['manage']['skip'], False)
+
+        # Request skip
+        response = self.client.put(self.url,
+                {
+                    'pause': False,
+                    'skip': True
+                }
+            )
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        # Get current commands
+        # Should be skip requested
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], True)
+
+        # Get player commands again but through aggregated route
+        response = self.client.get(self.url_aggregated)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['manage']['pause'], False)
+        self.assertEqual(response.data['manage']['skip'], True)
+
+
+        # Now, player is still playing the same song,
+        # server should send skip request
+        response = self.player_play_song(self.pe1.id)
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], True)
+
+        # Player now plays next song, skip flag should be reset
+        response = self.player_play_next_song()
+        self.assertEqual(response.data['pause'], False)
+        self.assertEqual(response.data['skip'], False)
+
+
+# TODO: Player errors check
---
+++ dakara_server/playlist/tests_playlist_entry.py
@@ -0,0 +1,161 @@
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .base_test import BaseAPITestCase
+from library.models import *
+from .models import *
+
+UserModel = get_user_model()
+
+class PlaylistEntryListCreateAPIViewTestCase(BaseAPITestCase):
+    url = reverse('playlist-list')
+
+    def setUp(self):
+        self.create_test_data()
+
+    def test_get_playlist_entries_list(self):
+        """
+        Test to verify playlist entries list
+        """
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Get playlist entries list 
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 2)
+        self.assertEqual(len(response.data['results']), 2)
+
+        # Playlist entries are in order of creation
+        self.check_playlist_entry_json(response.data['results'][0], self.pe1)
+        self.check_playlist_entry_json(response.data['results'][1], self.pe2)
+
+    def test_get_users_list_forbidden(self):
+        """
+        Test to verify users list is not available when not logged in
+        """
+        # Get users list
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_post_create_playlist_entry(self):
+        """
+        Test to verify playlist entry creation
+        """
+        # Login as playlist user
+        self.authenticate(self.p_user)
+
+        # Post new playlist entry
+        response = self.client.post(self.url, {"song": self.song1.id})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check playlist entry has been created in database
+        self.assertEqual(PlaylistEntry.objects.count(), 3)
+        new_entry = PlaylistEntry.objects.order_by('-date_created')[0]
+        # Entry was created with for song1
+        self.assertEqual(new_entry.song.id, self.song1.id)
+        # Entry's owner is the user who created it
+        self.assertEqual(new_entry.owner.id, self.p_user.id)
+
+    def test_post_create_user_forbidden(self):
+        """
+        Test to verify simple user cannot create playlist entries
+        """
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Attempt to post new playlist entry
+        response = self.client.post(self.url, {"song": self.song1.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_playlist_entries_list_playing_entry(self):
+        """
+        Test to verify playlist entries list does not include playing song
+        """
+        # Simulate a player playing next song
+        self.player_play_next_song()
+
+        # Login as simple user 
+        self.authenticate(self.user)
+
+        # Get playlist entries list 
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(len(response.data['results']), 1)
+
+        # Playlist entries are in order of creation
+        self.check_playlist_entry_json(response.data['results'][0], self.pe2)
+
+class PlaylistEntryRetrieveUpdateDestroyAPIViewTestCase(BaseAPITestCase):
+
+    def setUp(self):
+        self.create_test_data()
+
+        # Create urls to access these playlist entries
+        self.url_pe1 = reverse('playlist-detail', kwargs={"pk": self.pe1.id})
+        self.url_pe2 = reverse('playlist-detail', kwargs={"pk": self.pe2.id})
+
+
+    def test_delete_playlist_entry_manager(self):
+        """
+        Test to verify playlist entry deletion as playlist manager
+        """
+        # Login as playlist manager
+        self.authenticate(self.manager)
+
+        # Delete playlist entries created by manager
+        response = self.client.delete(self.url_pe1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # This playlist entry has been removed from database
+        self.assertEqual(PlaylistEntry.objects.count(), 1)
+        entries = PlaylistEntry.objects.filter(id=self.pe1.id)
+        self.assertEqual(len(entries), 0)
+
+        # Delete playlist entries created by other user
+        response = self.client.delete(self.url_pe2)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # This playlist entry has been removed from database
+        self.assertEqual(PlaylistEntry.objects.count(), 0)
+
+    def test_delete_playlist_entry_playlist_user(self):
+        """
+        Test to verify playlist entry deletion as playlist user
+        """
+        # Login as playlist user
+        self.authenticate(self.p_user)
+
+        # Delete playlist entries created by self
+        response = self.client.delete(self.url_pe2)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # This playlist entry has been removed from database
+        self.assertEqual(PlaylistEntry.objects.count(), 1)
+        entries = PlaylistEntry.objects.filter(id=self.pe2.id)
+        self.assertEqual(len(entries), 0)
+
+        # Attempt to delete playlist entry created by other user
+        response = self.client.delete(self.url_pe1)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_playlist_entry_playing(self):
+        """
+        Test to verify playing entry can not be deleted
+        """
+        # Simulate a player playing next song
+        self.player_play_next_song()
+
+        # Login as playlist manager
+        self.authenticate(self.manager)
+
+        # Attempt to delete playing entry 
+        response = self.client.delete(self.url_pe1)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # This playlist entry is still there
+        self.assertEqual(PlaylistEntry.objects.count(), 2)
+        entries = PlaylistEntry.objects.filter(id=self.pe1.id)
+        self.assertEqual(len(entries), 1)
