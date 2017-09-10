6b7e75096675d306c647606026fbb6411b827103
Nadeflore <flore.love@gmail.com>
2017-09-10 18:35:02 +0200

    Add local settings file
    Add default config loaded when no local settings file is found.

--- dakara_server/dakara_server/settings.py
+++ dakara_server/dakara_server/settings.py
@@ -16,17 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hc85nuu)6iu0z6!=5_y=0norbd)g)$syxy*9kw+3np@=0x)#k4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -89,23 +78,8 @@ CACHES = {
 WSGI_APPLICATION = 'dakara_server.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -155,3 +129,8 @@ LOGGING = {
         },
     },
 }
+
+try:
+    from .local_settings import *
+except ImportError:
+    from .default_settings import *
