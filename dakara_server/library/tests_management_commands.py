45ab757b5d7b7e41b3c0a8625d189fbcc70195cf
Nadeflore <flore.love@gmail.com>
2017-09-15 08:56:49 +0200

    Add feed command tests

--- dakara_server/library/tests_management_commands.py
+++ dakara_server/library/tests_management_commands.py
@@ -1,7 +1,7 @@
-import tempfile
+import tempfile, os, shutil
 from django.core.management import call_command
 from django.test import TestCase
-from .models import WorkType, SongTag
+from .models import WorkType, SongTag, Song, Artist
 
 class QueryLanguageParserTestCase(TestCase):
 
@@ -30,7 +30,7 @@ class QueryLanguageParserTestCase(TestCase):
         call_command('createtags', *args, **opts)
 
         # Post-Assertions
-        tags = SongTag.objects.all().order_by('name')
+        tags = SongTag.objects.order_by('name')
         self.assertEqual(len(tags), 2)
         self.assertEqual(tags[0].name, "TAGNAME1")
         self.assertEqual(tags[0].color_id, 0)
@@ -42,7 +42,7 @@ class QueryLanguageParserTestCase(TestCase):
         Test create work types command
         """
         # Pre-Assertions
-        work_types = WorkType.objects.all().order_by('query_name')
+        work_types = WorkType.objects.order_by('query_name')
         self.assertEqual(len(work_types), 0)
 
         file_content = """worktypes:
@@ -64,7 +64,7 @@ class QueryLanguageParserTestCase(TestCase):
         call_command('createworktypes', *args, **opts)
 
         # Post-Assertions
-        work_types = WorkType.objects.all().order_by('query_name')
+        work_types = WorkType.objects.order_by('query_name')
         self.assertEqual(len(work_types), 2)
         self.assertEqual(work_types[0].query_name, "work-type-one")
         self.assertEqual(work_types[0].name, "Work type one")
@@ -72,3 +72,89 @@ class QueryLanguageParserTestCase(TestCase):
         self.assertEqual(work_types[1].query_name, "work-type-two")
         self.assertEqual(work_types[1].name, "Work type two")
         self.assertEqual(work_types[1].icon_name, "cat")
+
+    def test_feed_command(self):
+        """
+        Test feed command
+        """
+        # Pre-Assertions
+        songs = Song.objects.order_by('title')
+        self.assertEqual(len(songs), 0)
+
+        dirpath = tempfile.mkdtemp()
+        try:
+            dirname = os.path.basename(dirpath)
+
+            first_file_filename = "The first file.mp4"
+            with open(os.path.join(dirpath, first_file_filename), 'wt') as f:
+                f.write("This is supposed to be an mp4 file content")
+
+            second_file_filename = "The second file.mp4"
+            with open(os.path.join(dirpath, second_file_filename), 'wt') as f:
+                f.write("This is supposed to be an mp4 file content")
+
+            # Call command
+            args = [dirpath]
+            opts = {}
+            call_command('feed', *args, **opts)
+
+            songs = Song.objects.order_by('title')
+            self.assertEqual(len(songs), 2)
+            self.assertEqual(songs[0].filename, first_file_filename)
+            self.assertEqual(songs[0].directory, dirname)
+            self.assertEqual(songs[0].title, os.path.splitext(first_file_filename)[0])
+            self.assertEqual(songs[1].filename, second_file_filename)
+            self.assertEqual(songs[1].directory, dirname)
+            self.assertEqual(songs[1].title, os.path.splitext(second_file_filename)[0])
+
+        finally:
+            shutil.rmtree(dirpath)
+
+    def test_feed_command_with_parser(self):
+        """
+        Test feed command with parser
+        """
+        # Pre-Assertions
+        songs = Song.objects.order_by('title')
+        self.assertEqual(len(songs), 0)
+        artists = Artist.objects.order_by('name')
+        self.assertEqual(len(artists), 0)
+
+        dirpath = tempfile.mkdtemp()
+        try:
+            dirname = os.path.basename(dirpath)
+
+            first_file_filename = "The first song name - Artist name.mp4"
+            with open(os.path.join(dirpath, first_file_filename), 'wt') as f:
+                f.write("This is supposed to be an mp4 file content")
+
+            second_file_filename = "The second song name - Artist name.mp4"
+            with open(os.path.join(dirpath, second_file_filename), 'wt') as f:
+                f.write("This is supposed to be an mp4 file content")
+
+            # Call command
+            args = [dirpath]
+            opts = {'parser': "library/parser_test.py"}
+            call_command('feed', *args, **opts)
+
+
+            artists = Artist.objects.order_by('name')
+            self.assertEqual(len(artists), 1)
+            artist = artists[0]
+            self.assertEqual(artist.name, "Artist name")
+
+            songs = Song.objects.order_by('title')
+            self.assertEqual(len(songs), 2)
+            self.assertEqual(songs[0].filename, first_file_filename)
+            self.assertEqual(songs[0].directory, dirname)
+            self.assertEqual(songs[0].title, "The first song name")
+            self.assertEqual(len(songs[0].artists.all()), 1)
+            self.assertEqual(songs[0].artists.all()[0].id, artist.id)
+            self.assertEqual(songs[1].filename, second_file_filename)
+            self.assertEqual(songs[1].directory, dirname)
+            self.assertEqual(songs[1].title, "The second song name")
+            self.assertEqual(len(songs[1].artists.all()), 1)
+            self.assertEqual(songs[1].artists.all()[0].id, artist.id)
+
+        finally:
+            shutil.rmtree(dirpath)
