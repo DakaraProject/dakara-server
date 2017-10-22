87f1abc70ddcbd18e6517cc74261cab7786b9fd3
Nadeflore <flore.love@gmail.com>
2017-10-22 22:58:48 +0200

    Add version file and display version

--- dakara_server/dakara_server/settings.py
+++ dakara_server/dakara_server/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from .version import __version__ as VERSION, __date__ as DATE
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Application definition
