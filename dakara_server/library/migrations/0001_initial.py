99ab960720419241b5a4e03ae7af9b635d2a8415
Nadeflore <flore.love@gmail.com>
2017-11-05 20:09:44 +0100

    Reset model migrations

--- dakara_server/library/migrations/0001_initial.py
+++ dakara_server/library/migrations/0001_initial.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import datetime
+import library.fields
 
 
 class Migration(migrations.Migration):
@@ -11,13 +13,83 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Artist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Song',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=255)),
-                ('file_path', models.CharField(max_length=255)),
+                ('filename', models.CharField(max_length=255)),
+                ('directory', models.CharField(max_length=255)),
+                ('duration', models.DurationField(default=datetime.timedelta(0))),
+                ('version', models.CharField(blank=True, max_length=255)),
+                ('detail', models.CharField(blank=True, max_length=255)),
+                ('detail_video', models.CharField(blank=True, max_length=255)),
+                ('lyrics', models.TextField(blank=True)),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('date_updated', models.DateTimeField(auto_now=True)),
+                ('artists', models.ManyToManyField(to='library.Artist')),
             ],
+        ),
+        migrations.CreateModel(
+            name='SongTag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', library.fields.UpperCaseCharField(max_length=255)),
+                ('color_id', models.IntegerField(null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='SongWorkLink',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('link_type', models.CharField(max_length=2, choices=[('OP', 'Opening'), ('ED', 'Ending'), ('IN', 'Insert song'), ('IS', 'Image somg')])),
+                ('link_type_number', models.IntegerField(null=True)),
+                ('episodes', models.CharField(blank=True, max_length=255)),
+                ('song', models.ForeignKey(to='library.Song')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Work',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('subtitle', models.CharField(blank=True, max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='WorkType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('name_plural', models.CharField(max_length=255)),
+                ('query_name', models.CharField(max_length=255, unique=True)),
+                ('icon_name', models.CharField(null=True, max_length=255)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='work',
+            name='work_type',
+            field=models.ForeignKey(null=True, to='library.WorkType'),
+        ),
+        migrations.AddField(
+            model_name='songworklink',
+            name='work',
+            field=models.ForeignKey(to='library.Work'),
+        ),
+        migrations.AddField(
+            model_name='song',
+            name='tags',
+            field=models.ManyToManyField(to='library.SongTag'),
+        ),
+        migrations.AddField(
+            model_name='song',
+            name='works',
+            field=models.ManyToManyField(through='library.SongWorkLink', to='library.Work'),
         ),
     ]
--- dakara_server/users/migrations/0001_initial.py
+++ dakara_server/users/migrations/0001_initial.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import users.models
 import django.utils.timezone
 import django.core.validators
+import users.models
 
 
 class Migration(migrations.Migration):
@@ -17,23 +17,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='DakaraUser',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
-                ('last_login', models.DateTimeField(verbose_name='last login', blank=True, null=True)),
-                ('is_superuser', models.BooleanField(verbose_name='superuser status', default=False, help_text='Designates that this user has all permissions without explicitly assigning them.')),
-                ('username', models.CharField(verbose_name='username', max_length=30, unique=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], error_messages={'unique': 'A user with that username already exists.'})),
-                ('first_name', models.CharField(verbose_name='first name', max_length=30, blank=True)),
-                ('last_name', models.CharField(verbose_name='last name', max_length=30, blank=True)),
-                ('email', models.EmailField(verbose_name='email address', max_length=254, blank=True)),
-                ('is_staff', models.BooleanField(verbose_name='staff status', default=False, help_text='Designates whether the user can log into this admin site.')),
-                ('is_active', models.BooleanField(verbose_name='active', default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
-                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
-                ('groups', models.ManyToManyField(verbose_name='groups', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group')),
-                ('user_permissions', models.ManyToManyField(verbose_name='user permissions', blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission')),
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, blank=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', default=False, verbose_name='superuser status')),
+                ('username', models.CharField(unique=True, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], max_length=30, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', error_messages={'unique': 'A user with that username already exists.'})),
+                ('first_name', models.CharField(max_length=30, blank=True, verbose_name='first name')),
+                ('last_name', models.CharField(max_length=30, blank=True, verbose_name='last name')),
+                ('email', models.EmailField(max_length=254, blank=True, verbose_name='email address')),
+                ('is_staff', models.BooleanField(help_text='Designates whether the user can log into this admin site.', default=False, verbose_name='staff status')),
+                ('is_active', models.BooleanField(help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', default=True, verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('users_permission_level', models.CharField(max_length=1, null=True, choices=[('u', 'User'), ('m', 'Manager')])),
+                ('library_permission_level', models.CharField(max_length=1, null=True, choices=[('u', 'User'), ('m', 'Manager')])),
+                ('playlist_permission_level', models.CharField(max_length=1, null=True, choices=[('p', 'Player'), ('u', 'User'), ('m', 'Manager')])),
+                ('groups', models.ManyToManyField(to='auth.Group', verbose_name='groups', related_query_name='user', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', blank=True, related_name='user_set')),
+                ('user_permissions', models.ManyToManyField(to='auth.Permission', verbose_name='user permissions', related_query_name='user', help_text='Specific permissions for this user.', blank=True, related_name='user_set')),
             ],
             options={
-                'verbose_name': 'user',
                 'verbose_name_plural': 'users',
+                'verbose_name': 'user',
                 'abstract': False,
             },
             managers=[
