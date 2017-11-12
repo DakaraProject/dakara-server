a1ab9478b6ea9e7b07c2d8c8a97f534d161d124b
Neraste <neraste.herr10@gmail.com>
2017-11-12 18:33:48 +0100

    Script to create archive

--- bump_version.sh
+++ bump_version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# server version
 if [[ -z $1 ]]
 then
     >&2 echo 'Error: no version specified'
@@ -9,14 +10,23 @@ fi
 version_number=$1
 version_date=$(date -I -u)
 
-cat <<EOF >dakara_server/dakara_server/version.py
+# patch version file
+version_file=dakara_server/dakara_server/version.py
+cat <<EOF >$version_file
 __version__ = '$version_number'
 __date__ = '$version_date'
 EOF
 
+# patch changelog
+changelog_file=CHANGELOG.md
 sed -i "/^## Unreleased$/a \\
 \\
-## $version_number - $version_date" CHANGELOG.md 
+## $version_number - $version_date" $changelog_file
 
+# create commit and tag
+git add $version_file $changelog_file
+git commit -m "Version $version_number" --no-verify
+git tag $version_number
+
+# say something
 echo "Version bumped to $version_number"
-
---
+++ create_archive.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# strict mode
+set -eu
+
+# create temporary directory
+tmpdir=$(mktemp -dt dakara_server_build.XXXXXX)
+
+# server version
+if [[ -z $1 ]]
+then
+    >&2 echo 'Error: no version specified'
+    exit 1
+fi
+
+version_number=$1
+archive_prefix=dakara-server_$version_number
+archive_name=$archive_prefix.zip
+
+# client version
+if [[ -z $2 ]]
+then
+    >&2 echo 'Error: no front version specified'
+    exit 1
+fi
+
+front_version_number=$2
+front_archive_name=dakara-client-web_$front_version_number.zip
+front_archive_url=https://github.com/DakaraProject/dakara-client-web/releases/download/$front_version_number/$front_archive_name
+
+# get client in temporary directory
+wget -q $front_archive_url -O $tmpdir/$front_archive_name
+mkdir -p $tmpdir/$archive_prefix/dakara_server
+unzip -q $tmpdir/$front_archive_name -d $tmpdir/$archive_prefix/dakara_server/
+
+# make server archive
+git archive \
+        --format zip \
+        --prefix $archive_prefix/ \
+        --worktree-attributes \
+        --output $archive_name \
+        refs/tags/$version_number
+
+# add client to server archive
+curdir=$PWD
+(cd $tmpdir; zip -qr $curdir/$archive_name $archive_prefix)
+
+# say something
+echo "Archive created in $archive_name"
+
+# delete temporary directory
+rm -rf $tmpdir
