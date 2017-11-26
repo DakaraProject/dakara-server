dd31f3fb69961bddacbbf9d3f358068c6caeedf6
Neraste <neraste.herr10@gmail.com>
2017-11-26 14:26:17 +0100

    Minor: change import order

--- dakara_server/playlist/views_front.py
+++ dakara_server/playlist/views_front.py
@@ -2,12 +2,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import (
         RetrieveUpdateDestroyAPIView,
         ListCreateAPIView,
         )
-
-from rest_framework.permissions import IsAuthenticated
 
 from . import models
 from . import serializers
