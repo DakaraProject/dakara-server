a4fbc829b161bf7067c092bf5e57db979115e05f
Neraste <neraste.herr10@gmail.com>
2018-05-18 18:54:09 +0200

    Fix migration script

--- dakara_server/users/migrations/0002_auto_20180518_1532.py
+++ dakara_server/users/migrations/0002_auto_20180518_1532.py
@@ -17,17 +17,17 @@ class Migration(migrations.Migration):
             model_name='dakarauser',
             name='username',
             field=models.CharField(
-                error_messages={'unique': ('A user with that username ',
-                                           'already exists.')},
-                help_text=('Required. 30 characters or fewer. Letters, digits '
-                           'and @/./+/-/_ only.'),
+                error_messages={'unique': 'A user with that username \
+already exists.'},
+                help_text='Required. 30 characters or fewer. Letters, \
+digits and @/./+/-/_ only.',
                 max_length=30,
                 unique=True,
                 validators=[
                     django.core.validators.RegexValidator(
                         '^[\\w.@+-]+$',
-                        ('Enter a valid username. This value may contain '
-                         'only letters, numbers and @/./+/-/_ characters.')
+                        'Enter a valid username. This value may contain \
+only letters, numbers and @/./+/-/_ characters.'
                     )
                 ],
                 verbose_name='username'
