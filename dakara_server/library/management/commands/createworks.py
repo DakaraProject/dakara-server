ba82d20b15a43dfec52f12a98400958171c0658d
Benstales <laviebenja@gmail.com>
2018-07-21 11:16:34 +0200

    Fix code to work on a JSON data structure.

--- dakara_server/library/management/commands/createworks.py
+++ dakara_server/library/management/commands/createworks.py
@@ -3,33 +3,21 @@ import sys
 import logging
 import importlib
 
-import progressbar
 from django.core.management.base import BaseCommand
 
 from library.models import (WorkType, WorkAlternativeTitle, Work)
 
 from .feed_components import default_work_parser
-# from .feed_components.progress_bar import TextProgressBar, TextNullBar
-
-# TODO set the progress bar (?)
-
-# wrap a special stream for warnings
-#
-# The wrapping done by progressbar seems to reassign the ouput and flush it
-# when needed, and not automatically. If the standard error is wrapped, it
-# mutes any exception, which is not acceptable. So, we create a custom wrapped
-# stream and assign warnings to use it since we cannot specify a new stream, we
-# use stderr for that, and reassign it to its origineal value right after.
-origin_stderr = sys.stderr
-wrapped_stderr = progressbar.streams.wrap_stderr()
-sys.stderr = origin_stderr
 
 # get logger
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-# hack the logger handler to use the wrapped stderr
-if logger.handlers:
-    logger.handlers[0].stream = wrapped_stderr
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s %(message)s")
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
 
 class WorkCreator:
@@ -37,7 +25,7 @@ class WorkCreator:
     database provided a work file and a parser.
 
     Args:
-        work_file (str): Path of the file storing the works data.
+        work_file (str): Absolute path of the file storing the works data.
         parser (module): Custom python module used to extract data from file.
         dry_run (bool): Run script in test mode.
 
@@ -55,106 +43,100 @@ class WorkCreator:
     def __init__(
             self,
             work_file,
-            parser,
-            dry_run=False):
-        self.dry_run = dry_run
-        # get works data
+            parser):
         self.works = parser.parse_work(work_file)
         self.work_alt_title_listing = []
 
-    # TODO restrict search fields
     @staticmethod
     def check_parser_result(dict_work):
         """Check if a work is correctly structured
+
+        Return
+            None if the structure, otherwise the field not recognized.
         """
+        field_names = ('subtitle', 'alternative_titles')
+
+        has_correct_struct = True
         field_error = None
         for field in dict_work:
-            if field not in Work._meta.fields:
+            if field not in field_names:
+                has_correct_struct = False
                 field_error = field
                 break
 
-        return field_error
+        return has_correct_struct, field_error
 
-    def save(self, obj):
-        """Save an object in database depending of the options."""
-        if self.dry_run:
-            obj.show(sys.stdout)
-        else:
-            obj.save()
-
-    def creatework(self, query_name, title_work, dict_work):
+    def creatework(self, work_type_entry, work_title, dict_work):
         """Create or update a work in database."""
         # check that the work data is well structured
-        field_error = self.check_parser_result(dict_work)
-        if field_error is not None:
+        has_correct_struct, field_error = self.check_parser_result(dict_work)
+        if not has_correct_struct:
             logger.warning(
                 "Incorrect field '{}' for '{}'".format(
                     field_error,
-                    title_work))
+                    work_title))
             return
 
         work_entry, work_created = Work.objects.get_or_create(
-            work__title__iexact=title_work
+            title__iexact=work_title
         )
 
         if work_created:
-            logger.debug("Created work '{}'".format(title_work))
+            logger.info("Created work '{}'".format(work_title))
 
         # get title
-        work_entry.title = title_work
+        work_entry.title = work_title
+
+        # get work type
+        work_entry.work_type = work_type_entry
 
         # get subtitle
         if dict_work['subtitle']:
             work_entry.subtitle = dict_work['subtitle']
 
-        # get work type
-        try:
-            work_type_entry = WorkType.objects.get(
-                query_name=query_name
-            )
-
-            work_entry.work_type = work_type_entry
-
-        except WorkType.DoesNotExist:
-            logger.warning("""Unable to find work type query name '{}'.
-                            Use createworktypes command first to create \
-                                    work types.""".format(query_name))
-
         # get work alternative titles or create them
         self.work_alt_title_listing = []
         for alt_title in dict_work['alternative_titles']:
-            self.create_alternative_title(title_work, alt_title)
+            self.create_alternative_title(work_entry, work_title, alt_title)
 
             if self.work_alt_title_listing:
-                logger.debug("Created alternative titles '{}' for '{}'".format(
-                        "', '".join(self.work_alt_title_listing), title_work))
+                logger.info("Created alternative titles '{}' for '{}'".format(
+                        "', '".join(self.work_alt_title_listing), work_title))
 
         # save work in the database
-        self.save(work_entry)
+        work_entry.save()
 
-    def create_alternative_title(self, title_work, alt_title):
-        work_alt_title_entry, work_alt_title_created = WorkAlternativeTitle.get_or_create(  # noqa E501
-            title__iexact=alt_title,
-            work__title__iexact=title_work
-            )
-
-        work_alt_title_entry.title = alt_title
-        work_alt_title_entry.work = title_work
-
-        # save work alternative title in the database
-        self.save(work_alt_title_entry)
+    def create_alternative_title(self, work_entry, work_title, alt_title):
+        work_alt_title_entry, work_alt_title_created = WorkAlternativeTitle.objects.get_or_create( # noqa E501
+                    title=alt_title,
+                    work__title=work_title,
+                    defaults={'work': work_entry}
+                )
 
         if work_alt_title_created:
+            # add to the listing the alternative title created
             self.work_alt_title_listing.append(alt_title)
 
     def createworks(self):
         """Create or update works provided."""
         # get works or create it
-        for query_name, dict_work_type in self.works:
-            for title_work, dict_work in dict_work_type:
-                self.creatework(query_name, title_work, dict_work)
+        for worktype_query_name, dict_work_type in self.works.items():
 
-        logger.debug("Works successfully created.")
+            # get work type
+            try:
+                work_type_entry = WorkType.objects.get(
+                        query_name=worktype_query_name
+                        )
+            except WorkType.DoesNotExist:
+                logger.error("""Unable to find work type query name '{}'.
+                            Use createworktypes command first to create \
+                                    work types.""".format(worktype_query_name))
+                continue
+
+            for work_title, dict_work in dict_work_type.items():
+                self.creatework(work_type_entry, work_title, dict_work)
+
+        logger.info("Works successfully created.")
 
 
 class Command(BaseCommand):
@@ -178,18 +160,14 @@ class Command(BaseCommand):
             default=None
         )
 
-        parser.add_argument(
-            "-r",
-            "--dry-run",
-            help="Run script in test mode, don't save anything in database.",
-            action="store_true"
-        )
-
     def handle(self, *args, **options):
         """Process the feeding
         """
         # work file data
-        work_file = os.path.normpath(options['work-file'])
+        work_file = os.path.join(
+                os.getcwd(),
+                os.path.normpath(options['work-file'])
+                )
 
         # parser
         if options.get('parser'):
@@ -207,8 +185,7 @@ class Command(BaseCommand):
 
         work_creator = WorkCreator(
                 work_file,
-                parser,
-                dry_run=options.get('dry_run'))
+                parser)
 
         # run the work creator
         work_creator.createworks()
