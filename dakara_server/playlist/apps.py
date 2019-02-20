56e2eaebe5d311d6daab97843f707d94c94c8a2b
Neraste <neraste.herr10@gmail.com>
2019-02-20 15:21:21 +0900

    Avoid double call of `ready` with development server

--- dakara_server/playlist/apps.py
+++ dakara_server/playlist/apps.py
@@ -1,3 +1,5 @@
+import os
+
 from django.apps import AppConfig
 
 
@@ -10,6 +12,17 @@ class PlaylistConfig(AppConfig):
     def ready(self):
         """Method called when app start
         """
+        # When the server is run with the `runserver` command, two instances of
+        # the project are running and hence this method is called twice: one
+        # for the reloader and one for the actual development server. The
+        # reloader creates the environment variable `RUN_MAIN`, so it can be
+        # distinguighed.
+        # See answers of https://stackoverflow.com/q/33814615
+        # See django/utils/autoreload.py
+        if "RUN_MAIN" in os.environ:
+            return
+
+        # The code bellow cannot be executed by the reloader
         from playlist.date_stop import check_date_stop_on_app_ready
 
         check_date_stop_on_app_ready()
