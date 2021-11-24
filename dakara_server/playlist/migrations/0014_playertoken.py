1fba4beb940bbfaf9d04386eaa28f9f6b829c156
Neraste <neraste.herr10@gmail.com>
2021-11-25 00:28:53 +0900

    Fix migrations

--- dakara_server/playlist/migrations/0014_playertoken.py
+++ dakara_server/playlist/migrations/0014_playertoken.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("playlist", "0012_playlistentry_use_instrumental"),
+        ("playlist", "0013_player"),
     ]
 
     operations = [
