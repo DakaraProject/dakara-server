143af1b895a833b59a73c4898801e4ad42d4bc21
Neraste <neraste.herr10@gmail.com>
2025-02-16 19:54:31 +0100

    Add pyproject.toml and change project structure

--- src/dakara_server/__init__.py
+++ src/dakara_server/__init__.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
 import os
 import sys
 
-if __name__ == "__main__":
+
+def django_manage():
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE", "dakara_server.settings.development"
     )
