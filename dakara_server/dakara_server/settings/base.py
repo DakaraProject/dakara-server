49e5f90892984878f42e2c750aef87b367f984d7
Neraste <neraste.herr10@gmail.com>
2025-02-23 15:49:30 +0100

    Fix websocket management for Daphne

--- dakara_server/dakara_server/settings/base.py
+++ dakara_server/dakara_server/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = (
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "daphne",
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
--- dakara_server/playlist/signals.py
+++ dakara_server/playlist/signals.py
@@ -1,20 +1,27 @@
+from threading import Event
+
 from django.db.backends.signals import connection_created
 from django.dispatch import receiver
 
 from internal.reloader import is_reloader
+
+connection_created_once = Event()
 
 
 @receiver(connection_created)
 def handle_connection_created(connection, **kwargs):
     """Perform playlist initialization operations as soon as the database is
     ready."""
-    from playlist.date_stop import check_date_stop_on_app_ready
-    from playlist.models import clean_channel_names
+    if not connection_created_once.is_set():
+        connection_created_once.set()
 
-    clean_channel_names()
+        from playlist.date_stop import check_date_stop_on_app_ready
+        from playlist.models import clean_channel_names
 
-    if is_reloader():
-        return
+        clean_channel_names()
 
-    # not called by the reloader
-    check_date_stop_on_app_ready()
+        if is_reloader():
+            return
+
+        # not called by the reloader
+        check_date_stop_on_app_ready()
