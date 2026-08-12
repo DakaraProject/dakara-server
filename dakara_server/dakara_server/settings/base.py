4f8f167678f1e2889539c1b06a0076df3536c4f8
Neraste <neraste.herr10@gmail.com>
2026-08-12 23:18:10 +0200

    Increase playlist limit to 1000 entries

--- dakara_server/dakara_server/settings/base.py
+++ dakara_server/dakara_server/settings/base.py
@@ -158,7 +158,7 @@ def get_host_urls(host_url):
 
 
 # limit of the playlist size
-PLAYLIST_SIZE_LIMIT = config("DAKARA_PLAYLIST_SIZE_LIMIT", cast=int, default=100)
+PLAYLIST_SIZE_LIMIT = config("DAKARA_PLAYLIST_SIZE_LIMIT", cast=int, default=1000)
 
 # interval of the scheduler (in minutes)
 SCHEDULER_INTERVAL = config("DAKARA_SCHEDULER_INTERVAL", cast=int, default=5)
